d7df301910465dd70eae1486ae2601eafed7f813
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-02 09:19:36 +0530

    Updated Code

---
+++ JavaProgram2/Parrotprogram/src/com/bridgelabz/Parrotprogram/Bird.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.Parrotprogram;
+
+public class  Bird {
+	static String name;
+    protected String colour;
+    String species;
+	
+     void fly() {
+	        System.out.println("Bird  can fly.");
+	        
+	    }
+
+	    void swim() {
+	        System.out.println("Bird can or  cannot swim.");
+	    }        
+	        
+	}
+
